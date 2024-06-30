8f26ee2bcbd6051e11fd6539e0e92ad94c672b4e
Xabi07 <garveyliam7@gmail.com>
2024-06-30 13:02:41 +0000

    Finbonachi series using recursive loop

--- main.cpp
+++ main.cpp
@@ -5,23 +5,41 @@ int next_term = 0;
 int max_term = 10;
 
 // main() runs in its own thread in the OS]
-/*Simple For Loop to execute the Fibonacci sequence, the first term in the sequence is added to the next term upto a limit,
-the limit here in this example is a max of 10 terms in the sequence. This code has some added printf statements to understand what gets printed to 
-screen when
+/*Added a recursive loop to execute the Finbonachi sequence, the recursive loop is called using the "fib(i)" statement, 
+when "fib(i)" is called the program goes to the function "int Fib(int n)", here the Finbonachi series is executed similar to the previous
+example, just some slightly different logic is used to implement the same sequence, this program prints both 
+version of the sequence out to the terminal
 */
-int main()
+int fib(int n){
+    if(n==0)
+        return 0;
+    if(n==1)
+        return 1;
+    else 
+        return(fib(n-1) + fib(n-2));    
+
+}
+
+int main() 
 {
-    printf("Finbonachi series: %d,%d, \r\n", t_1, t_2);
-    for(int i = 0; i < max_term; i ++){
+    printf("Finbonanchi series recursive loop: ");
+    for(int i = 0; i < max_term; i++){
+        printf("%d, ", fib(i));
+    }
+    printf("\r\n");
+    printf("Finbonachi series simple loop: %d,%d, ", t_1, t_2);
+    for(int i = 2; i < max_term; i ++){
         next_term = t_1 + t_2;
         printf("%d, " , next_term);
         t_1 = t_2;
         t_2 = next_term;
-    }
-    printf("what does this do\n");
-    printf("just a note\r\n");
-    //while (true) {
 
-    //}
+    }
+    printf("\r\n");
+    //printf("what does this do\n");
+    //printf("just a note\r\n");
+    while (true) {
+
+    }
 }
 
